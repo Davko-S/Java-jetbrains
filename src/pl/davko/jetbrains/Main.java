aa36cc1914b84a8056a92ef0d685ad7e3cd4e76a
Davko <66025445+Davko-S@users.noreply.github.com>
2020-11-11 23:22:33 +0100

    Added class with methods overriding

--- src/pl/davko/jetbrains/Main.java
+++ src/pl/davko/jetbrains/Main.java
@@ -11,22 +11,6 @@ import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int sum = 0;
-        int divisor = 0;
-        double avg;
-
-        for (int i = a; i <= b; i++) {
-            if (i % 3 == 0) {
-                sum = sum + i;
-                divisor++;
-            }
-        }
-        avg = (double) sum / divisor;
-        System.out.println(avg);
     }
 }
 
