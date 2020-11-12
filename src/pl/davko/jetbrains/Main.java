9c1bae983ffb167b60c34c180a033b7b205e34ab
Davko <66025445+Davko-S@users.noreply.github.com>
2020-11-12 22:14:42 +0100

    Changed package names and practice with Employee class

--- src/pl/davko/jetbrains/Main.java
+++ src/pl/davko/jetbrains/Main.java
@@ -7,10 +7,13 @@ import java.util.Random;
 import java.util.Scanner;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import pl.davko.jetbrains.excercise.*;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        Employee employee = new Employee("John", "john@email.com", 5);
+        System.out.println(employee.getEmail());
     }
 }
 
