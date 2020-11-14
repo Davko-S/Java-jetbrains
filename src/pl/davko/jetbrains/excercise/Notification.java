cd2e2e61950a2e08095bcccde52bcda516ce95e6
Davko <66025445+Davko-S@users.noreply.github.com>
2020-11-14 22:09:54 +0100

    Added Notification class for polymorphism practice

---
+++ src/pl/davko/jetbrains/excercise/Notification.java
@@ -0,0 +1,41 @@
+package pl.davko.jetbrains.excercise;
+
+public class Notification {
+    protected String msg;
+
+    public Notification(String msg) {
+        this.msg = msg;
+    }
+
+    public void show() {
+        System.out.println(getMsg());
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
+
+class Warning extends Notification {
+
+    public Warning(String msg) {
+        super(msg);
+    }
+    @Override
+    public String getMsg() {
+        return "WARN: " + msg;
+    }
+}
+
+class Alarm extends Notification {
+
+    public Alarm(String msg) {
+        super(msg);
+    }
+    @Override
+    public void show() {
+        System.out.println("ALARM: " + msg);
+    }
+
+
+}
