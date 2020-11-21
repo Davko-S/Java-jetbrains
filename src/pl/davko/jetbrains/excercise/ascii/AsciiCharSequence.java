a7edba720c8ba76579435fa22aec525fcc2849dd
Davko <66025445+Davko-S@users.noreply.github.com>
2020-11-22 00:28:50 +0100

    Added AsciiCharSequence class which implements CherSequence interf.

---
+++ src/pl/davko/jetbrains/excercise/ascii/AsciiCharSequence.java
@@ -0,0 +1,35 @@
+package pl.davko.jetbrains.excercise.ascii;
+
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+public class AsciiCharSequence implements java.lang.CharSequence {
+
+    byte[] ascii;
+
+
+    public AsciiCharSequence(byte[] ascii) {
+        this.ascii = ascii.clone();
+    }
+
+
+    @Override
+    public int length() {
+        return ascii.length;
+    }
+
+    @Override
+    public char charAt(int index) {
+        return (char) ascii[index];
+    }
+
+    @Override
+    public CharSequence subSequence(int start, int end) {
+        return new AsciiCharSequence(Arrays.copyOfRange(ascii, start, end));
+    }
+
+    @Override
+    public String toString() {
+        return new String(ascii.clone());
+    }
+}
