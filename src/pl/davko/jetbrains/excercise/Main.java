bf923a87f54d008450d36ee4b32c2d63049fd6c9
Davko <66025445+Davko-S@users.noreply.github.com>
2020-11-21 22:50:21 +0100

    Added exemplary Interface with all possible members

--- src/pl/davko/jetbrains/excercise/Main.java
+++ src/pl/davko/jetbrains/excercise/Main.java
@@ -14,30 +14,7 @@ import pl.davko.jetbrains.excercise.baseentity.*;
 
 public class Main {
     public static void main(String[] args) {
-        User user = new User();
-        user.setName("John");
-
-        BaseEntity userEntity = user;
-        userEntity.setId(100);
-        userEntity.setVersion(1);
-
-        WebSite site = new WebSite();
-        site.setUrl("https://hyperskill.org");
-
-        BaseEntity siteEntity = site;
-        siteEntity.setId(101);
-        siteEntity.setVersion(1);
-
-        Visit visit = new Visit();
-        visit.setUser(user);
-        visit.setSite(site);
-
-        BaseEntity baseVisit = visit;
-        baseVisit.setId(102);
-        baseVisit.setVersion(103);
-
-        System.out.println(-30 / 7);
-
+        
     }
 }
 
