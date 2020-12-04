ce55dab3afc5cba9a0efe9d8e8bb828c215216b2
Davko <66025445+Davko-S@users.noreply.github.com>
2020-12-04 22:28:40 +0100

    Added Game class to solve the task

--- src/pl/davko/jetbrains/excercise/Main.java
+++ src/pl/davko/jetbrains/excercise/Main.java
@@ -15,27 +15,9 @@ import pl.davko.jetbrains.excercise.strategy.*;
 import pl.davko.jetbrains.excercise.factory.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-        final Scanner scanner = new Scanner(System.in);
 
-        final String type = scanner.next();
-        Time time = null;
-
-        switch (type) {
-            case "noon":
-                time = Time.noon();
-                break;
-            default:
-                time = null;
-                break;
-        }
-
-        if (time == null) {
-            System.out.println(time);
-        } else {
-            System.out.println(String.format("%s %s %s", time.hour, time.minute, time.second));
-        }
     }
 }
 
