ad69d345240f380b2027f2cb2f0adeba4552914a
Davko <66025445+Davko-S@users.noreply.github.com>
2020-12-19 23:10:31 +0100

    Solved POTD with substring() in Main

--- src/pl/davko/jetbrains/excercise/Main.java
+++ src/pl/davko/jetbrains/excercise/Main.java
@@ -13,13 +13,17 @@ import pl.davko.jetbrains.excercise.factory.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        TableStore tableStore = new TableStore();
-        Table strangeTable = tableStore.orderTable("Mysterious table");
-        Table officeTable = tableStore.orderTable("office");
-        Table kitchenTable = tableStore.orderTable("kitchen");
-        Table officeTable2 = tableStore.orderTable("Office");
-        Table labTable = tableStore.orderTable("lab");
+        String word = scanner.next();
+        String result = "";
+
+        if (word.length() > 2 && word.length() % 2 == 0) {
+            result = word.substring(0, word.length() / 2 - 1) + word.substring(word.length() / 2 + 1);
+            } else if (word.length() > 2) {
+            result = word.substring(0, (word.length() - 1) / 2) + word.substring((word.length() - 1) / 2 + 1);
+        }
+        System.out.println(result);
     }
 }
 
