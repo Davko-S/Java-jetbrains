386c6c9ce27a571f3c1f97eed8a2a080524d44d1
Davko <66025445+Davko-S@users.noreply.github.com>
2021-01-07 22:59:50 +0100

    Printed MagicNumber

--- src/pl/davko/jetbrains/excercise/Main.java
+++ src/pl/davko/jetbrains/excercise/Main.java
@@ -2,17 +2,25 @@ package pl.davko.jetbrains.excercise;
 
 import pl.davko.jetbrains.excercise.factory.burger.BurgerStore;
 
+import static pl.davko.jetbrains.excercise.potd.MagicNumber.printStar;
+import static pl.davko.jetbrains.excercise.potd.MagicNumber.printVoid;
+
 public class Main {
     public static void main(String[] args) {
 
-        BurgerStore burgerStore = new BurgerStore();
-        try {
-            burgerStore.orderBurger("chinese");
-            burgerStore.orderBurger("american");
-            burgerStore.orderBurger("russian");
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        printStar(8);
+        System.out.println("");
+        printVoid(6); printStar(2);
+        System.out.println("");
+        printVoid(6); printStar(2);
+        System.out.println("");
+        printVoid(6); printStar(2);
+        System.out.println("");
+        printStar(2); printVoid(4); printStar(2);
+        System.out.println("");
+        printStar(2); printVoid(4); printStar(2);
+        System.out.println("");
+        printVoid(1); printStar(7);
     }
 }
 
---
+++ src/pl/davko/jetbrains/excercise/potd/MagicNumber.java
@@ -0,0 +1,16 @@
+package pl.davko.jetbrains.excercise.potd;
+
+public class MagicNumber {
+
+    public static void printStar(int amount) {
+        for (int i = 0; i < amount; i++) {
+            System.out.print('*');
+        }
+    }
+
+    public static void printVoid(int amount) {
+        for (int i = 0; i < amount; i++) {
+            System.out.print(' ');
+        }
+    }
+}
