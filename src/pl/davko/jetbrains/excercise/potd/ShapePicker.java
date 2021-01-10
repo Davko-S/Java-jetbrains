132c53c56c6e67e4780744e7ac33b5dcc4216fb8
Davko <66025445+Davko-S@users.noreply.github.com>
2021-01-10 23:41:38 +0100

    Enhanced switch statement in ShapePicker

--- src/pl/davko/jetbrains/excercise/potd/ShapePicker.java
+++ src/pl/davko/jetbrains/excercise/potd/ShapePicker.java
@@ -9,16 +9,11 @@ public class ShapePicker {
         int shape = scanner.nextInt();
 
         switch (shape) {
-            case 1: System.out.println("You have chosen a square");
-            break;
-            case 2: System.out.println("You have chosen a circle");
-            break;
-            case 3: System.out.println("You have chosen a triangle");
-            break;
-            case 4: System.out.println("You have chosen a rhombus");
-            break;
-            default: System.out.println("There is no such shape!");
-            break;
+            case 1 -> System.out.println("You have chosen a square");
+            case 2 -> System.out.println("You have chosen a circle");
+            case 3 -> System.out.println("You have chosen a triangle");
+            case 4 -> System.out.println("You have chosen a rhombus");
+            default -> System.out.println("There is no such shape!");
         }
     }
 }
