f17600b7dcee94f9b67fa8359b4e5154ddbda528
Davko <66025445+Davko-S@users.noreply.github.com>
2021-01-12 22:19:50 +0100

    Interface fields in Message subclasses

--- src/pl/davko/jetbrains/excercise/cipherApp/EncryptedMessage.java
+++ src/pl/davko/jetbrains/excercise/cipherApp/EncryptedMessage.java
@@ -2,7 +2,11 @@ package pl.davko.jetbrains.excercise.cipherApp;
 
 public class EncryptedMessage extends Message {
 
+    Encrypt performEncryption;
+
     public EncryptedMessage(String data, String inFile, String outFile, String alg, boolean isOut, int key) {
         super(data, inFile, outFile, alg, isOut, key);
     }
+
+
 }
