3dc98b62dbb36556af6f86a3323173daf34652d9
Davko <66025445+Davko-S@users.noreply.github.com>
2021-01-13 13:04:58 +0100

    IntMultiplication solved potd

--- src/pl/davko/jetbrains/excercise/Main.java
+++ src/pl/davko/jetbrains/excercise/Main.java
@@ -1,11 +1,21 @@
 package pl.davko.jetbrains.excercise;
 
+import pl.davko.jetbrains.excercise.potd.IntMultiplication;
 import pl.davko.jetbrains.excercise.potd.NutsForSquirrels;
 import pl.davko.jetbrains.excercise.potd.VowelChecker;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(VowelChecker.checkVowel('i'));
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        long product = 1;
+        for (int i = a; i < b; i++) {
+            product = product * i;
+        }
+        System.out.println(product);
     }
 }
 
