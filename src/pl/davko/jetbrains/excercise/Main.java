c77e8cf4ba9acd393c7536fa49cd9d59034bfa29
Davko <66025445+Davko-S@users.noreply.github.com>
2021-01-14 09:42:13 +0100

    Class Book in .potd

--- src/pl/davko/jetbrains/excercise/Main.java
+++ src/pl/davko/jetbrains/excercise/Main.java
@@ -8,14 +8,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        long product = 1;
-        for (int i = a; i < b; i++) {
-            product = product * i;
-        }
-        System.out.println(product);
+
     }
 }
 
