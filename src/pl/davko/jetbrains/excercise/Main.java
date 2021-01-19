5151e30933b436e4e6c7c4f2202590e32a58645f
Davko <66025445+Davko-S@users.noreply.github.com>
2021-01-19 22:51:49 +0100

    Iterating over Arrays in Battleship project

--- src/pl/davko/jetbrains/excercise/Main.java
+++ src/pl/davko/jetbrains/excercise/Main.java
@@ -1,11 +1,10 @@
 package pl.davko.jetbrains.excercise;
 
-import pl.davko.jetbrains.excercise.factory.motor.Motor;
-import pl.davko.jetbrains.excercise.factory.motor.MotorStaticFactory;
+import pl.davko.jetbrains.excercise.basics.ArrayForEach;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(ArrayForEach.containsTwoInt());
     }
 }
 
---
+++ src/pl/davko/jetbrains/excercise/basics/ArrayForEach.java
@@ -0,0 +1,45 @@
+package pl.davko.jetbrains.excercise.basics;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayForEach {
+
+    public static void addGreaterThan() {
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+//        int[] array = Arrays.stream(scanner.nextLine().split(" "))
+//                .mapToInt(Integer::parseInt)
+//                .toArray();
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+        int n = scanner.nextInt();
+        int sum = 0;
+        for (int a: array) {
+            if (a > n) {
+                sum += a;
+            }
+        }
+        System.out.println(sum);
+    }
+
+    public static boolean containsTwoInt() {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] array = new int[scanner.nextInt()];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
+        }
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        for (int i = 1; i < array.length; i++) {
+            if (n == array[i - 1] && m == array[i] || n == array[i] && m == array[i - 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
