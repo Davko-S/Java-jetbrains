61ca57dc622dc6a0ca6e401c3c2a3219403992d7
Davko <66025445+Davko-S@users.noreply.github.com>
2021-01-20 20:14:57 +0100

    Final implementation of rightRotation()

--- src/pl/davko/jetbrains/excercise/basics/arrays/RightRotation.java
+++ src/pl/davko/jetbrains/excercise/basics/arrays/RightRotation.java
@@ -7,26 +7,32 @@ public class RightRotation {
 
     public static void rightRotation() {
         Scanner scanner = new Scanner(System.in);
+
+        //*** int[] passed as Stream from Scanner(System.in)
         int[] array = Arrays.stream(scanner.nextLine().split(" "))
                 .mapToInt(Integer::parseInt)
                 .toArray();
 
+        //*** Setting variables for rotation and result int[]
         int rotation = scanner.nextInt();
         int[] newArray = new int[array.length];
 
+
+        //*** Core program function: index gets changed x rotation
         for (int i = 0; i < array.length; i++) {
             int newIndex;
             if (array[i] + rotation >= array.length) {
                 newIndex = (i + rotation) % array.length;
             } else {
-                newIndex = (i + rotation);
+                newIndex = i + rotation;
             }
             newArray[newIndex] = array[i];
         }
+
         //*** Printing using Arrays.toString()
         System.out.println(Arrays.toString(array));
         System.out.println(Arrays.toString(newArray));
-        
+
         //*** Printing using for each loop
         for (int before: array) {
             System.out.print(before + " ");
