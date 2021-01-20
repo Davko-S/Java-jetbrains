7d6b16d92fb5abeded274478cb9f145383e8eb0e
Davko <66025445+Davko-S@users.noreply.github.com>
2021-01-20 21:00:28 +0100

    make3dArray() implemented for multidim. arrays

--- src/pl/davko/jetbrains/excercise/Main.java
+++ src/pl/davko/jetbrains/excercise/Main.java
@@ -1,11 +1,12 @@
 package pl.davko.jetbrains.excercise;
 
 import pl.davko.jetbrains.excercise.basics.arrays.ArrayForEach;
+import pl.davko.jetbrains.excercise.basics.arrays.MultiDimension;
 import pl.davko.jetbrains.excercise.basics.arrays.RightRotation;
 
 public class Main {
     public static void main(String[] args) {
-        RightRotation.rightRotation();
+        MultiDimension.make3dArray(2, 5, 10);
     }
 }
 
---
+++ src/pl/davko/jetbrains/excercise/basics/arrays/MultiDimension.java
@@ -0,0 +1,51 @@
+package pl.davko.jetbrains.excercise.basics.arrays;
+
+import java.util.Arrays;
+
+public class MultiDimension {
+
+    public static void make3dArray(int arrays, int rows, int fields) {
+
+        int[][][] cubic = new int[arrays][rows][fields];
+
+        //*** Keeping the value for entries in the first array
+        int current = 1;
+
+        //*** Filling the fields of each row of each array
+        for (int i = 0; i < arrays; i++) {
+            for (int j = 0; j < rows; j++) {
+                for (int k = 0; k < fields; k++) {
+                    cubic[i][j][k] = current;
+                }
+            }
+            current++;
+        }
+
+        //*** Printing out the fields in blocks (matrix)
+        for (int i = 0; i < arrays; i++) {
+            for (int j = 0; j < rows; j++) {
+                for (int k = 0; k < fields; k++) {
+                    System.out.print(cubic[i][j][k] + " ");
+                }
+                System.out.println("");
+            }
+            System.out.println("");
+        }
+
+        //*** Alternative with foreach loop
+        /*for (int[][] dim2Array : cubic) {     // for each 2-dim array
+            for (int[] vector : dim2Array) {  // for each 1-dim array (vector) of 2-dim array
+                Arrays.fill(vector, current); // fill the vector
+            }
+            current++; // the next current
+        }
+
+        // this code prints all 2-dimensional arrays
+        for (int[][] dim2Array : cubic) {
+            for (int[] vector : dim2Array) {
+                System.out.println(Arrays.toString(vector));
+            }
+            System.out.println();
+        }*/
+    }
+}
