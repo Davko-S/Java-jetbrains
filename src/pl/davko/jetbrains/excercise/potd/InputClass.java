c99849d2b321ad7dd937500c26d0c8fb1b083a49
Davko <66025445+Davko-S@users.noreply.github.com>
2021-01-21 23:25:15 +0100

    Fully qualified class name vs. static import of Scanner class

---
+++ src/pl/davko/jetbrains/excercise/potd/InputClass.java
@@ -0,0 +1,9 @@
+package pl.davko.jetbrains.excercise.potd;
+
+public class InputClass {
+    public static String getString() {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+}
