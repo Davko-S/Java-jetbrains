1ff427615a129d604dc12919d44f816f6e7e3b08
Davko <66025445+Davko-S@users.noreply.github.com>
2021-01-23 23:09:03 +0100

    Implemented sumOfNeighbours() to sum 2D array

--- src/pl/davko/jetbrains/excercise/basics/arrays/Neighbours.java
+++ src/pl/davko/jetbrains/excercise/basics/arrays/Neighbours.java
@@ -1,5 +1,6 @@
 package pl.davko.jetbrains.excercise.basics.arrays;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -7,22 +8,40 @@ public class Neighbours {
 
     public static void sumOfNeighbours() {
         Scanner scanner = new Scanner(System.in);
+
+        //Pass first line of input as String and then continue within while loop
         String input = scanner.nextLine();
 
+        //Initializing an ArrayList to be able to add rows dynamically depending on user's input
+        ArrayList<int[]> arrayRow = new ArrayList<int[]>();
 
+        //Loop which changes input String via String[] to int[] array and makes calculations
         while (!input.equals("end")) {
+
             String[] numbers = input.split(" ");
             int[] array = new int[numbers.length];
             for (int i = 0; i < array.length; i++) {
                 array[i] = Integer.parseInt(numbers[i]);
             }
-            for (int n : array) {
-                System.out.print(n + " ");
-            }
-            System.out.println();
+            //Adding each input line as a new row to ArrayList
+            arrayRow.add(array);
             input = scanner.nextLine();
         }
+        //Converting ArrayList to int[][]
+        int[][] array = arrayRow.toArray(new int[arrayRow.size()][]);
+        int[][] results = new int[array.length][array[0].length];
 
 
+        //Printing the results as output after calculations
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                results[i][j] = array[(i - 1 + array.length) % array.length][j % array[i].length]
+                        + array[(i + 1) % array.length][j % array[i].length]
+                        + array[i % array.length][(j - 1 + array[i].length) % array[i].length]
+                        + array[i % array.length][(j + 1) % array[i].length];
+                System.out.print(results[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
