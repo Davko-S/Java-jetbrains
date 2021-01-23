aceb0c18a6c5aedb940b5cf9e5b955d2a748b591
Davko <66025445+Davko-S@users.noreply.github.com>
2021-01-23 18:23:48 +0100

    Neighbours: input for int[] works

--- src/pl/davko/jetbrains/excercise/basics/arrays/Neighbours.java
+++ src/pl/davko/jetbrains/excercise/basics/arrays/Neighbours.java
@@ -1,18 +1,28 @@
 package pl.davko.jetbrains.excercise.basics.arrays;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Neighbours {
 
     public static void sumOfNeighbours() {
         Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
 
-        String input = "";
+
         while (!input.equals("end")) {
+            String[] numbers = input.split(" ");
+            int[] array = new int[numbers.length];
+            for (int i = 0; i < array.length; i++) {
+                array[i] = Integer.parseInt(numbers[i]);
+            }
+            for (int n : array) {
+                System.out.print(n + " ");
+            }
+            System.out.println();
             input = scanner.nextLine();
-
-            
         }
+
 
     }
 }
