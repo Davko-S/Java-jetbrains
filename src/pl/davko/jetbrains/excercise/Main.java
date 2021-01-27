2864dd19f59a4595271f795fd2b02ab103c8acb8
Davko <66025445+Davko-S@users.noreply.github.com>
2021-01-27 23:51:32 +0100

    countSequence() now works

--- src/pl/davko/jetbrains/excercise/Main.java
+++ src/pl/davko/jetbrains/excercise/Main.java
@@ -1,11 +1,12 @@
 package pl.davko.jetbrains.excercise;
 
+import pl.davko.jetbrains.excercise.basics.arrays.LongestSequence;
 import pl.davko.jetbrains.excercise.basics.arrays.Neighbours;
 import pl.davko.jetbrains.excercise.potd.Application;
 
 public class Main {
     public static void main(String[] args) {
-        
+        LongestSequence.countSequence();
     }
 }
 
--- src/pl/davko/jetbrains/excercise/basics/arrays/LongestSequence.java
+++ src/pl/davko/jetbrains/excercise/basics/arrays/LongestSequence.java
@@ -7,30 +7,30 @@ public class LongestSequence {
         Scanner scanner = new Scanner(System.in);
 
         int length = scanner.nextInt();     // Size of input array
-        String input = scanner.nextLine();
+        int[] ints = new int[length];
 
         if (length == 1) {
             System.out.println("1");
-        }
-
-        String[] numbers = input.split("");
-        int[] ints = new int[length];
-
-        for (int i = 0; i < length; i++) {
-            ints[i] = Integer.parseInt(numbers[i]);
-        }
-
-        // Count the length of ascending sequence
-        int oldCounter = 1;
-        int counter = 1;
-        for (int i = 1; i <= ints.length; i++) {
-            if (ints[i] > ints[i - 1]) {
-                counter++;
-            } else {
-                if (counter > oldCounter)
-                oldCounter = counter;
+        } else {
+            for (int i = 0; i < length; i++) {
+                ints[i] = scanner.nextInt();
+                System.out.print(ints[i] + " ");
             }
+            System.out.println();
+
+            // Count the length of ascending sequence
+            int oldCounter = 1;
+            int counter = 1;
+            for (int i = 1; i < ints.length; i++) {
+                if (ints[i] > ints[i - 1]) {
+                    counter++;
+                } else {
+                    if (counter > oldCounter)
+                        oldCounter = counter;
+                        counter = 1;
+                }
+            }
+            System.out.println(Math.max(counter, oldCounter));
         }
-        System.out.println(oldCounter);
     }
 }
