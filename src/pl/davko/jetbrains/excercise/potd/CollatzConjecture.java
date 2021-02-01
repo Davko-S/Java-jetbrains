68d173dd8ea1d7ba90094b4c15a039e04644d1b7
Davko <66025445+Davko-S@users.noreply.github.com>
2021-02-01 23:24:31 +0100

    CollatzConjecture simplified

--- src/pl/davko/jetbrains/excercise/potd/CollatzConjecture.java
+++ src/pl/davko/jetbrains/excercise/potd/CollatzConjecture.java
@@ -6,33 +6,11 @@ public class CollatzConjecture {
 
     public static void generateCollatz() {
         Scanner scanner = new Scanner(System.in);
-
-        int n = scanner.nextInt();
-        if (n == 1) {
-            System.out.println(n);
-        } else if (n <= 0 ) {
-            System.out.println("Please provide natural number!");
-        } else if (n == 2) {
-            System.out.println(n + " " + 1);
-        } else {
-            int next = 0;
-            System.out.print(n + " ");
-            if (n % 2 == 0) {
-                next = n / 2;
-            } else {
-                next = 3 * n + 1;
-            }
-            int newNext = next;
-            System.out.print(next + " ");
-            do {
-                if (next % 2 == 0) {
-                    newNext = next / 2;
-                } else {
-                    newNext = next * 3 + 1;
-                }
-                next = newNext;
-                System.out.print(newNext + " ");
-            } while (newNext != 1);
+        int num = scanner.nextInt();
+        System.out.print(num + " ");
+        while (num != 1) {
+            num = num % 2 == 0 ? num / 2 : num * 3 + 1;
+            System.out.print(num + " ");
         }
     }
 }
