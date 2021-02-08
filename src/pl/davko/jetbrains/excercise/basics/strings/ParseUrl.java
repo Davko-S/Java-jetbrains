eb7917a7745f976ce88a4c8a91cb23de64ddf2f9
Davko <66025445+Davko-S@users.noreply.github.com>
2021-02-08 11:59:04 +0100

    ParseUrl hard coded ver.

--- src/pl/davko/jetbrains/excercise/basics/strings/ParseUrl.java
+++ src/pl/davko/jetbrains/excercise/basics/strings/ParseUrl.java
@@ -14,6 +14,7 @@ public class ParseUrl {
         String[] cutTwo = cutOne[1].split("&");
         String pass = "";
         String port = "";
+        String name = "";
         String cookie = "";
         String host = "";
         String password = "";
@@ -22,11 +23,60 @@ public class ParseUrl {
         if (first[0].contains("pass")) {
             pass = first[1];
             password = pass;
+            try {
+                String[] portArray = cutTwo[1].split("=");
+                port = portArray[1];
+            } catch (ArrayIndexOutOfBoundsException e) {
+                port = "not found";
+            }
+            try {
+                String[] cookieArray = cutTwo[2].split("=");
+                if (cookieArray[0].equals("cookie")) {
+                    cookie = cookieArray[1];
+                } else if (cookieArray[0].equals("name")) {
+                    name = cookieArray[1];
+                }
+
+            } catch (ArrayIndexOutOfBoundsException f) {
+                cookie = "not found";
+            }
+            try {
+                String[] hostArray = cutTwo[3].split("=");
+                host = hostArray[1];
+            } catch (ArrayIndexOutOfBoundsException g) {
+                host = "not found";
+            }
         } else {
-            port = first[1];
+            try {
+                String[] portArray = cutTwo[0].split("=");
+                port = portArray[1];
+            } catch (ArrayIndexOutOfBoundsException e) {
+                port = "not found";
+            }
+            try {
+                String[] cookieArray = cutTwo[1].split("=");
+                cookie = cookieArray[1];
+            } catch (ArrayIndexOutOfBoundsException f) {
+                cookie = "not found";
+            }
+            try {
+                String[] hostArray = cutTwo[2].split("=");
+                host = hostArray[1];
+            } catch (ArrayIndexOutOfBoundsException g) {
+                host = "not found";
+            }
         }
-        System.out.println("pass: " + pass);
-        System.out.println("password: " + password);
-        System.out.println("port: " + port);
+        if (!pass.equals("")) {
+            System.out.println("pass : " + pass);
+        }
+        System.out.println("port : " + port);
+        if (!name.equals("")) {
+            System.out.println("name : " + name);
+        }
+        System.out.println("cookie : " + cookie);
+        System.out.println("host : " + host);
+        if (!password.equals("")) {
+            System.out.println("password : " + password);
+        }
     }
 }
