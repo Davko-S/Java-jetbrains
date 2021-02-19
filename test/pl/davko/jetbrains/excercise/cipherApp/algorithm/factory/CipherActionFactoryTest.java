ef7fcca072c73f58cd1ab9d52fa9a0617a0cd8a6
Davko <66025445+Davko-S@users.noreply.github.com>
2021-02-19 20:25:19 +0100

    Started Factory tests

---
+++ test/pl/davko/jetbrains/excercise/cipherApp/algorithm/factory/CipherActionFactoryTest.java
@@ -0,0 +1,12 @@
+package pl.davko.jetbrains.excercise.cipherApp.algorithm.factory;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CipherActionFactoryTest {
+
+    @Test
+    void make() {
+    }
+}
