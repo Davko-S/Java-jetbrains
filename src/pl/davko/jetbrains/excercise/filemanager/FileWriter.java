0f305d7c7143073594d4c7b19a6ba1db50efa152
Davko <66025445+Davko-S@users.noreply.github.com>
2023-03-29 23:02:07 +0200

    Formatter class part 2: TO DO list

--- src/pl/davko/jetbrains/excercise/filemanager/FileWriter.java
+++ src/pl/davko/jetbrains/excercise/filemanager/FileWriter.java
@@ -1,11 +1,16 @@
 package pl.davko.jetbrains.excercise.filemanager;
 
+import java.io.File;
+import java.text.Normalizer;
 import java.util.Formatter;
+import java.util.Scanner;
 
 ///Formatter class can be used to save contents into a file///
 
 public class FileWriter {
     public static void main(String[] args) {
+
+        /* THE BASICS: how it works
         try {
             Formatter newFormatter = new Formatter("contents_to_file.txt");
             newFormatter.format("%s %s %s", "1", "FUSE:", "Fusing objects to create weapons etc.\r\n");
@@ -13,7 +18,35 @@ public class FileWriter {
             newFormatter.close();
         } catch (Exception e) {
             System.out.println("Error");
-        }
+        }*/
 
+        // TO DO list exercise
+        Scanner input = new Scanner(System.in);
+        try {
+            Formatter f = new Formatter("tasks.txt");
+            int count = 0;
+            while(count < 3) {
+                f.format("%s", input.next() + "\n");
+                count++;
+            }
+            f.close();
+
+        } catch (Exception exception) {
+            System.out.println("Error");
+        }
+        readFile();
+    }
+    public static void readFile() {
+        try {
+            File x = new File("tasks.txt");
+            Scanner sc = new Scanner(x);
+            while(sc.hasNext()) {
+                System.out.println(sc.next());
+            }
+            sc.close();
+        }
+        catch (Exception e) {
+            System.out.println("Error");
+        }
     }
 }
